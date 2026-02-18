e08db966152a6f41d87cde995347f1b99d0b5691
ManideepArruri <manideeparruri@gmail.com>
2026-02-18 15:00:52 +0530

    Coded to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+// This App displays a simple message on the console as OOPS using the predefined System.out.println() method
+// @author Developer
+// @version 1
+
+
+
+class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println("OOPS");
+	}
+}
