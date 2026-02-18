69815348a92a233446d8928b19614dacbc449c58
ManideepArruri <manideeparruri@gmail.com>
2026-02-18 15:13:59 +0530

    Coded to Render OOPS as Banner using Print Statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+// This App displays a simple message on the console as OOPS using star patterns
+// @author Developer
+// @version 2
+
+class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println("  ***   "    +"  ***    "  +"***  " +"  ****");
+		System.out.println(" *   *  "    +" *   *   "  +"*  * " +" *");
+		System.out.println("*     * "    +"*     *  "  +"***  " +"****");
+		System.out.println(" *   *  "    +" *   *   "  +"*    " +"   *");
+		System.out.println("  ***   "    +"  ***    "  +"*    " +"****");
+	}
+}
