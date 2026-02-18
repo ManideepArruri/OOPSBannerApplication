386699ecd887778c9c3d2b84f7bda85d8a9ebaa2
ManideepArruri <manideeparruri@gmail.com>
2026-02-18 15:48:35 +0530

    Coded to Render OOPS as Banner using String.join() method

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+// This App displays a simple message on the console as OOPS using String.join() method
+// @author Developer
+// @version 3
+
+class OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println(String.join(" ","  ***   "    ,"  ***    "  ,"***  " ,"  ****"));
+		System.out.println(String.join(" "," *   *  "    ," *   *   "  ,"*  * " ," *"));
+		System.out.println(String.join(" ","*     * "    ,"*     *  "  ,"***  " ,"****"));
+		System.out.println(String.join(" "," *   *  "    ," *   *   "  ,"*    " ,"   *"));
+		System.out.println(String.join(" ","  ***   "    ,"  ***    "  ,"*    " ,"****"));
+		
+		System.out.println(String.join(" ","Hi","I","am","Manideep"));
+		
+	}
+}
